aae2ed3cdf370d175330ed1b1b77c4c6f9ba30b6
Netanel Friedman <netanel.friedman6@gmail.com>
2026-05-20 21:12:26 +0300

    day07: properly solve part 2

--- day07/main.sh
+++ day07/main.sh
@@ -43,51 +43,72 @@ function part2 {
     function rec_eval_node_sum {
         local node_name="$1"
 
-        if [[ -z "${node_sum["$node_name"]}" ]]; then
-
-            IFS=' '
-            local children_text="${node_children["$node_name"]}"
-            for child in $children_text; do
-                rec_eval_node_sum "$child"
-            done
-
-            local total="${node_value["$node_name"]}"
-            local children_text="${node_children["$node_name"]}"
-            for child in $children_text; do
-                local value="${node_sum["$child"]}"
-                total=$((total + value))
-            done
-
-            node_sum["$node_name"]="$total"
-
-            if [[ -z "$children_text" ]]; then
-                return
-            fi
+        if [[ -n "${node_sum["$node_name"]}" ]]; then
+            return
         fi
+
+        IFS=' '
+        local children_text="${node_children["$node_name"]}"
+        for child in $children_text; do
+            rec_eval_node_sum "$child"
+        done
+
+        local total="${node_value["$node_name"]}"
+        local children_text="${node_children["$node_name"]}"
+        for child in $children_text; do
+            local value="${node_sum["$child"]}"
+            total=$((total + value))
+        done
+
+        node_sum["$node_name"]="$total"
     }
 
     local parent="$(find_highest_parent)"
     rec_eval_node_sum "$parent"
 
-    # TODO: automate this to print out the odd one.
-    #       this can be done by going deeper to the non-similar child, until all children are equal.
-    #       compare the sums.
-    local queue="$parent"
-    local depth=0
-    while [[ -n "$queue" ]]; do
-        local new_queue=""
 
-        for node_name in $queue; do
-            local children="${node_children["$node_name"]}"
-            new_queue+=" $children"
+    heads=("$parent")
+    local head="${heads[0]}"
 
-            local indents=$((depth*4))
-            printf "%*s sum: ${node_sum["$node_name"]}, value: ${node_value["$node_name"]}, $node_name\n" "$indents" ""
-        done
-
-        queue="$new_queue"
-        depth=$((depth + 1))
+    while [[ -n "$head" ]]; do
+        heads+=("$head")
+        local children="${node_children["$head"]}"
+        head="$(find_odd_node "${children[@]}")"
     done
+
+    local target_name="${heads[-1]}"
+    local target_parent_name="${heads[-2]}"
+    local siblings="${node_children["$target_parent_name"]}"
+    siblings="${siblings/"$target"/}"
+    siblings="${siblings/  / }"
+    siblings=($siblings)
+
+    local sibling_name="${siblings[0]}"
+    local sibling_sum="${node_sum["$sibling_name"]}"
+    local target_sum="${node_sum["$target_name"]}"
+    local diff=$((sibling_sum - target_sum))
+    local target_value="${node_value["$target_name"]}"
+    local new_target_value=$((target_value + diff))
+    echo "$new_target_value"
+}
+
+function find_odd_node {
+    local names=($@)
+
+    declare -A value_to_name
+    declare -a values
+
+    for name in "${names[@]}"; do
+        local value="${node_sum["$name"]}"
+        value_to_name["$value"]="$name"
+        values+=("$value")
+        # echo "name: $name, value: $value"
+    done
+
+    local unique_value=$(IFS=$'\n'; echo "${values[*]}" | sort | uniq -u)
+    if [[ -n "$unique_value" ]]; then
+        echo "${value_to_name["$unique_value"]}"
+    fi
 }
 
 function find_highest_parent {
